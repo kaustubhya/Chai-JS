47ef7790fb4624ebce4fe45317687bf49bc59dea
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2024-07-04 12:09:26 +0000

    ..

--- JS_02/Database_and_ECMA_Standards.js
+++ JS_02/Database_and_ECMA_Standards.js
@@ -1,7 +1,6 @@
 // In JavaScript all the newer version JS Codes are written in the form of "use strict;"
 // This is a directive to the JavaScript engine to use the "strict mode"
 
-
 // There is nothing such as "use no strict" in JavaScript
 
 // alert( "Hello World!" );
@@ -44,17 +43,16 @@ eg. 2 to power 53, 2.569, -58, -12.215235 etc.
 Primitive Datatypes over
 */
 
-let age = 41
-console.log(typeof age)
-
+let age = 41;
+console.log(typeof age);
 
 // "typeof" is basically used to give us back the type of datatype
 
-console.log(typeof "Nunneo")
+console.log(typeof "Nunneo");
 
-console.log(typeof undefined)
+console.log(typeof undefined);
 
-console.log(typeof null)
+console.log(typeof null);
 
 /* ##################################################3
 Notice here for null, we get returned back an object, when we use typeof, for undefined, we get back undefined only
@@ -63,4 +61,5 @@ Notice here for null, we get returned back an object, when we use typeof, for un
 This tells us undefined is a type in itself whereas null is an object
 ############      INTERVIEW QUESTION        ###############
 
+This was initially a bug and most websites got made using this bug. So if they changed it then, then most websites would have been crashed, so they kept it as it is!!
 */
